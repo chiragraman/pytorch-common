a4cee0eedf0124a0695881414de35b4ba0190764
Chirag Raman <chirag.raman@gmail.com>
2020-03-22 12:40:43 +0100

    Add layers to z-encoder and update initialization

--- initialization.py
+++ initialization.py
@@ -9,10 +9,13 @@
 ###
 
 
+from typing import Callable
+
+import torch
 import torch.nn as nn
 
 
-def init_recurrent_weights(model):
+def init_recurrent_weights(model: nn.Module) -> None:
     """ Initialize the recurrent weights of the module
 
     Args:
@@ -25,7 +28,7 @@ def init_recurrent_weights(model):
             chunk_rows = param.shape[0] // n_chunks
             init_func(param[idx*chunk_rows:(idx+1)*chunk_rows])
 
-    chunks = {nn.GRU:3, nn.LSTM:4, nn.RNN:1}
+    chunks = {nn.GRU: 3, nn.LSTM: 4, nn.RNN: 1}
     for m in model.modules():
         if isinstance(m, (nn.GRU, nn.LSTM, nn.RNN)):
             n_chunks = chunks[type(m)]
@@ -36,3 +39,19 @@ def init_recurrent_weights(model):
                     init_recurrent_param(param, n_chunks, nn.init.orthogonal_)
                 elif "bias" in name:
                     param.data.fill_(0)
+
+
+def init_weight(
+        module: nn.Module, name: str,
+        init_func: Callable[[torch.Tensor, ...], None] = nn.init.normal_
+    ) -> None:
+    """ Apply init function to the weight parameter of the module matching name
+
+    Args:
+        model       :   The nn.Module containing modules to iterate over
+        name        :   The value of __class__.__name__ to match
+        init_func   :   The init function to apply
+    """
+    classname = module.__class__.__name__
+    if classname.find(name) != -1:
+        init_func(module.weight)
